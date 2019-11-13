Cleaned up embedding model code and fixed normalization axis problem.

python/eukg/emb/EmbeddingModel.py
@@ -214,17 +214,18 @@ class DistMult(TransE):
     r = self.embedding_lookup(rel)
     t = self.embedding_lookup(tail)
 
-    return self.energy_activation(tf.reduce_sum(h * r * t,
-                                                axis=-1,
-                                                keepdims=False))
+    pre_activation = tf.reduce_sum(h * r * t, axis=-1)
+    post_activation = self.energy_activation(pre_activation)
+    return post_activation
 
   def normalize_parameters(self):
     return tf.no_op()
 
   def regularization(self, c_parameters, r_parameters):
     reg_term = 0
-    for p in c_parameters + r_parameters:
-      reg_term += tf.reduce_sum(tf.norm(self.embedding_lookup(p)))
+    all_params = c_parameters + r_parameters
+    for p in all_params:
+      reg_term += tf.reduce_sum(tf.norm(self.embedding_lookup(p), axis=-1))
     return reg_term
 
 
@@ -329,9 +330,9 @@ class DistMultACE(BaseModel):
   def regularization(self, c_parameters, r_parameters):
     reg_term = 0
     for p in c_parameters:
-      reg_term += tf.reduce_sum(tf.norm(self.embedding_lookup(p, 'concept')))
+      reg_term += tf.reduce_sum(tf.norm(self.embedding_lookup(p, 'concept'), axis=-1))
     for p in r_parameters:
-      reg_term += tf.reduce_sum(tf.norm(self.embedding_lookup(p, 'rel')))
+      reg_term += tf.reduce_sum(tf.norm(self.embedding_lookup(p, 'rel'), axis=-1))
     return reg_term
 
   def embedding_lookup(self, ids, emb_type=None):