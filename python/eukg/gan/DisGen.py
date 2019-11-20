ea9435a1dbc2174950e30263ea9cea5e5074bda4
Max Weinzierl <maxwellweinzierl@gmail.com>
2019-11-19 18:59:16 -0600

    Working on single-pass gpu tensorflow model for GAN

--- python/eukg/gan/DisGen.py
+++ python/eukg/gan/DisGen.py
@@ -345,10 +345,10 @@ class DisGenGan(DisGen):
         e_neg_subj_proj, e_neg_obj_proj, e_pos_subj_proj, e_pos_obj_proj = un_flatten_gen(e_concepts_proj)
         # only take first negative sample for discriminator loss
         # TODO replace this index with g_sampl gather
-        e_neg_subj = tf.batch_gather(e_neg_subj, g_sampls)
-        e_neg_subj_proj = tf.batch_gather(e_neg_subj_proj, g_sampls)
-        e_neg_obj = tf.batch_gather(e_neg_obj, g_sampls)
-        e_neg_obj_proj = tf.batch_gather(e_neg_obj_proj, g_sampls)
+        e_neg_subj = tf.gather(e_neg_subj, g_sampls)
+        e_neg_subj_proj = tf.gather(e_neg_subj_proj, g_sampls)
+        e_neg_obj = tf.gather(e_neg_obj, g_sampls)
+        e_neg_obj_proj = tf.gather(e_neg_obj_proj, g_sampls)
 
         e_neg_subj = e_neg_subj, e_neg_subj_proj
         e_neg_obj = e_neg_obj, e_neg_obj_proj
@@ -357,8 +357,8 @@ class DisGenGan(DisGen):
       else:
         e_neg_subj, e_neg_obj, e_pos_subj, e_pos_obj = un_flatten_gen(e_concepts)
         # TODO replace this index with g_sampl gather
-        e_neg_subj = tf.batch_gather(e_neg_subj, g_sampls)
-        e_neg_obj = tf.batch_gather(e_neg_obj, g_sampls)
+        e_neg_subj = tf.gather(e_neg_subj, g_sampls)
+        e_neg_obj = tf.gather(e_neg_obj, g_sampls)
 
       return e_neg_subj, e_neg_obj, e_pos_subj, e_pos_obj
 
@@ -367,6 +367,8 @@ class DisGenGan(DisGen):
       self.g_sampls
     )
     d_e_rels = self.dis_embedding_model.embedding_lookup(self.relations, 'rel')
+    print(self.g_sampls.get_shape())
+    print(d_e_neg_subj.get_shape())
 
     with tf.variable_scope('dis_energy'):
       self.d_pos_energy = self.dis_embedding_model.energy_from_embeddings(
@@ -408,7 +410,7 @@ class DisGenGan(DisGen):
       self.discounted_reward = tf.stop_gradient(self.d_reward - self.baseline)
       # [batch_size, num_samples] - this is for sampling during GAN training
       self.g_probability_distributions = tf.nn.softmax(self.g_sampl_energies, axis=-1)
-      self.g_probabilities = tf.batch_gather(self.g_probability_distributions, self.g_sampls, name='sampl_probs')
+      self.g_probabilities = tf.gather(self.g_probability_distributions, self.g_sampls, name='sampl_probs')
       # TODO ask if self.discounted_reward should be [bsize] neg energies, then multiplied to each of these
       # TODO losses before sum/avg instead of sum and multiplying by avg neg energy of discriminator.
       g_loss = -tf.reduce_sum(tf.log(self.g_probabilities))
