7653c373a05b9e25493ef1ebc7a5dca46f07db24
Max Weinzierl <maxwellweinzierl@gmail.com>
2019-12-28 15:06:55 -0600

    Working on eval scripts

--- python/eukg/test/classification.py
+++ python/eukg/test/classification.py
@@ -41,7 +41,6 @@ def evaluate():
 
   model_name = config.run_name
 
-
   if config.gpu_memory_growth:
     gpu_config = tf.ConfigProto()
     gpu_config.gpu_options.allow_growth = True
@@ -52,32 +51,11 @@ def evaluate():
     tf.set_random_seed(config.seed)
     # init model
     # with tf.variable_scope(scope):
-    if config.ace_model:
-      t_data = data_util.load_metathesaurus_token_data(config.data_dir)
-      ace_model = AceModel.ACEModel(config, t_data)
-    else:
-      ace_model = None
-    model = train.init_model(config, None, ace_model)
-
-    if config.ace_model and not config.load and config.pre_run_name is not None:
-      pre_model_ckpt = tf.train.latest_checkpoint(
-        os.path.join(all_models_dir, config.model, config.pre_run_name))
-      ace_model.init_from_checkpoint(pre_model_ckpt)
+    model = train.init_model(config, None, eval=True, pairwise=True)
 
     tf.global_variables_initializer().run()
     tf.local_variables_initializer().run()
 
-    if config.ace_model:
-      ace_model.initialize_tokens(session)
-
-    if config.load:
-      # init saver
-      tf_saver = tf.train.Saver(max_to_keep=10)
-
-      # load model
-      ckpt = tf.train.latest_checkpoint(os.path.join(config.model_dir, config.model, model_name))
-      print('Loading checkpoint: %s' % ckpt)
-      tf_saver.restore(session, ckpt)
     tf.get_default_graph().finalize()
 
     scores = []
@@ -88,6 +66,7 @@ def evaluate():
                                                                             model.pos_obj: o,
                                                                             model.neg_subj: np.expand_dims(ns, axis=-1),
                                                                             model.neg_obj: np.expand_dims(no, axis=-1)})
+      nscores = nscores[:, 0]
       scores += pscores.tolist()
       labels += np.ones_like(pscores, dtype=np.int).tolist()
       scores += nscores.tolist()
@@ -111,6 +90,7 @@ def evaluate():
                                                                             model.pos_obj: o,
                                                                             model.neg_subj: np.expand_dims(ns, axis=-1),
                                                                             model.neg_obj: np.expand_dims(no, axis=-1)})
+      nscores = nscores[:, 0]
       scores += pscores.tolist()
       labels += np.ones_like(pscores, dtype=np.int).tolist()
       scores += nscores.tolist()
@@ -124,88 +104,5 @@ def evaluate():
     print(metrics.classification_report(labels, predictions))
 
 
-def evaluate_sn():
-  random.seed(config.seed)
-  np.random.seed(config.seed)
-  config.no_semantic_network = False
-
-  data = {}
-  cui2id, _, _, _ = data_util.load_metathesaurus_data(config.data_dir, 0.)
-  _ = data_util.load_semantic_network_data(config.data_dir, data)
-  subj, rel, obj = data['sn_subj'], data['sn_rel'], data['sn_obj']
-  print('Loaded %d sn triples from %s' % (len(rel), config.data_dir))
-
-  valid_triples = set()
-  for trip in zip(subj, rel, obj):
-    valid_triples.add(trip)
-  print('%d valid triples' % len(valid_triples))
-  idxs = np.random.permutation(np.arange(len(rel)))
-  idxs = idxs[:600]
-  subj, rel, obj = subj[idxs], rel[idxs], obj[idxs]
-  sampler = DataGenerator.NegativeSampler(valid_triples=valid_triples, name='???')
-  nsubj, nobj = sampler.sample(subj, rel, obj)
-
-  model_name = config.run_name
-  if config.mode == 'gan':
-    scope = config.dis_run_name
-    model_name += '/discriminator'
-    config.mode = 'disc'
-  else:
-    scope = config.run_name
-
-  with tf.Graph().as_default(), tf.Session() as session:
-    # init model
-    with tf.variable_scope(scope):
-      model = train.init_model(config, None)
-
-    tf.global_variables_initializer().run()
-    tf.local_variables_initializer().run()
-
-    # init saver
-    tf_saver = tf.train.Saver(max_to_keep=10)
-
-    # load model
-    ckpt = tf.train.latest_checkpoint(os.path.join(config.model_dir, config.model, model_name))
-    print('Loading checkpoint: %s' % ckpt)
-    tf_saver.restore(session, ckpt)
-    tf.get_default_graph().finalize()
-
-    feed_dict = {model.smoothing_placeholders['sn_relations']: rel,
-                 model.smoothing_placeholders['sn_pos_subj']: subj,
-                 model.smoothing_placeholders['sn_pos_obj']: obj,
-                 model.smoothing_placeholders['sn_neg_subj']: nsubj,
-                 model.smoothing_placeholders['sn_neg_obj']: nobj}
-    pscores, nscores = session.run([model.sn_pos_energy, model.sn_neg_energy], feed_dict)
-    scores = np.concatenate((pscores, nscores))
-    labels = np.concatenate((np.ones_like(pscores, dtype=np.int), np.zeros_like(nscores, dtype=np.int)))
-    print('Calculated scores. Training SVM.')
-    svm = LinearSVC(dual=False)
-    svm.fit(scores.reshape(-1, 1), labels)
-    print('Done.')
-
-    with np.load(os.path.join(config.data_dir, 'semnet', 'triples.npz')) as npz:
-      subj = npz['subj']
-      rel = npz['rel']
-      obj = npz['obj']
-    nsubj, nobj = sampler.sample(subj, rel, obj)
-    feed_dict = {model.smoothing_placeholders['sn_relations']: rel,
-                 model.smoothing_placeholders['sn_pos_subj']: subj,
-                 model.smoothing_placeholders['sn_pos_obj']: obj,
-                 model.smoothing_placeholders['sn_neg_subj']: nsubj,
-                 model.smoothing_placeholders['sn_neg_obj']: nobj}
-    pscores, nscores = session.run([model.sn_pos_energy, model.sn_neg_energy], feed_dict)
-    predictions = svm.predict(pscores.reshape(-1, 1)).tolist()
-    labels = np.ones_like(pscores, dtype=np.int).tolist()
-    predictions += svm.predict(nscores.reshape(-1, 1)).tolist()
-    labels += np.zeros_like(nscores, dtype=np.int).tolist()
-    print('SN Relation Triple Classification Accuracy:  %.4f' % metrics.accuracy_score(labels, predictions))
-    print('SN Relation Triple Classification Precision: %.4f' % metrics.precision_score(labels, predictions))
-    print('SN Relation Triple Classification Recall:    %.4f' % metrics.recall_score(labels, predictions))
-    print(metrics.classification_report(labels, predictions))
-
-
 if __name__ == "__main__":
-  if config.eval_mode == 'sn':
-    evaluate_sn()
-  else:
-    evaluate()
+  evaluate()
