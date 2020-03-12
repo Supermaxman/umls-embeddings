740ef827e954665d1c26b596b5fc328cbe142a18
supermaxman <maxwellweinzierl@gmail.com>
2020-03-12 17:39:22 -0500

    Training settings

--- python/bin/train_ace.sh
+++ python/bin/train_ace.sh
@@ -778,3 +778,99 @@ python -m python.eukg.train \
 --num_workers=6 \
 --buffer_size=1 \
 --load=False
+
+python -m python.eukg.train \
+--mode=disgen \
+--model=transd-distmult \
+--run_name=transd-dm-disgen-cake-ext-cased-sep-4 \
+--ace_model \
+--bert_config='/users/max/data/models/bert/biobert_v1.1_pubmed/bert_config.json' \
+--bert_vocab='/users/max/data/models/bert/biobert_v1.1_pubmed/vocab.txt' \
+--encoder_checkpoint='/users/max/data/models/bert/biobert_v1.1_pubmed/bert_model.ckpt' \
+--energy_activation='none' \
+--shared_encoder=False \
+--num_atom_samples=0 \
+--no_semantic_network \
+--encoder_rnn_layers=1 \
+--encoder_rnn_size=512 \
+--encoder_rnn_type=lstm \
+--embedding_size=100 \
+--gamma=0.5 \
+--energy_norm_ord=2 \
+--learning_rate=1e-5 \
+--batch_size=64 \
+--val_batch_size=64 \
+--num_epochs=10 \
+--data_dir=/media/mdrive/umls-embeddings-ext \
+--secondary_data_dir=/media/mdrive/umls-embeddings-ext \
+--model_dir=/users/max/data/models/umls-embeddings \
+--summaries_dir=/shared/hltdir4/disk1/max/logs \
+--num_workers=6 \
+--buffer_size=1 \
+--load=False
+
+python -m python.eukg.train \
+--mode=gan-joint \
+--model=transd-distmult \
+--run_name=transd-dm-gan-joint-cake-ext-cased-sep-4 \
+--pre_run_name=transd-dm-disgen-cake-ext-cased-sep-4 \
+--ace_model \
+--bert_config='/users/max/data/models/bert/biobert_v1.1_pubmed/bert_config.json' \
+--bert_vocab='/users/max/data/models/bert/biobert_v1.1_pubmed/vocab.txt' \
+--encoder_checkpoint='/users/max/data/models/bert/biobert_v1.1_pubmed/bert_model.ckpt' \
+--energy_activation='none' \
+--shared_encoder=False \
+--num_atom_samples=0 \
+--no_semantic_network \
+--encoder_rnn_layers=1 \
+--encoder_rnn_size=512 \
+--encoder_rnn_type=lstm \
+--embedding_size=100 \
+--gamma=0.5 \
+--energy_norm_ord=2 \
+--learning_rate=1e-5 \
+--batch_size=64 \
+--val_batch_size=64 \
+--num_epochs=10 \
+--baseline_type=avg_prev_batch_momentum \
+--baseline_momentum=0.9 \
+--data_dir=/media/mdrive/umls-embeddings-ext \
+--secondary_data_dir=/media/mdrive/umls-embeddings-ext \
+--model_dir=/users/max/data/models/umls-embeddings \
+--summaries_dir=/shared/hltdir4/disk1/max/logs \
+--num_workers=6 \
+--buffer_size=1 \
+--load=False
+
+python -m python.eukg.train \
+--mode=gan-joint \
+--model=transd-distmult \
+--run_name=transd-dm-gan-joint-cake-ext-cased-sep-5 \
+--pre_run_name=transd-dm-disgen-cake-ext-cased-sep-4 \
+--ace_model \
+--bert_config='/users/max/data/models/bert/biobert_v1.1_pubmed/bert_config.json' \
+--bert_vocab='/users/max/data/models/bert/biobert_v1.1_pubmed/vocab.txt' \
+--encoder_checkpoint='/users/max/data/models/bert/biobert_v1.1_pubmed/bert_model.ckpt' \
+--energy_activation='none' \
+--shared_encoder=False \
+--num_atom_samples=0 \
+--no_semantic_network \
+--encoder_rnn_layers=1 \
+--encoder_rnn_size=512 \
+--encoder_rnn_type=lstm \
+--embedding_size=100 \
+--gamma=0.5 \
+--energy_norm_ord=2 \
+--learning_rate=1e-5 \
+--batch_size=16 \
+--val_batch_size=16 \
+--num_epochs=10 \
+--baseline_type=avg_prev_batch_momentum \
+--baseline_momentum=0.99 \
+--data_dir=/media/mdrive/umls-embeddings-ext \
+--secondary_data_dir=/media/mdrive/umls-embeddings-ext \
+--model_dir=/users/max/data/models/umls-embeddings \
+--summaries_dir=/shared/hltdir4/disk1/max/logs \
+--num_workers=6 \
+--buffer_size=1 \
+--load=False
